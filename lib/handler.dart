1c4c0d3479d0e48b06b18a5ab80fcefa9b43844b
Sylvanus Kateile <kateile@outlook.com>
2021-09-23 23:24:50 +0300

    fix extracting pages

---
+++ lib/handler.dart
@@ -0,0 +1,69 @@
+import 'table.dart';
+
+class Topic {
+  final String title;
+  final String index;
+  final bool isChapter;
+  final int page;
+
+  Topic({
+    required this.title,
+    required this.index,
+    this.isChapter = false,
+    required this.page,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      "index": index,
+      "title": title,
+      "page": page,
+      "isChapter": isChapter,
+    };
+  }
+
+  @override
+  String toString() {
+    return toJson().toString();
+  }
+}
+
+List<Topic> handlePages() {
+  final List<Topic> list = [];
+  final strings = tableOfContents.split("\n");
+
+  for (var i in strings) {
+    final word = i.trim();
+
+    final String index = word.split(" ").firstWhere(
+          (e) => e.contains(RegExp("[0-9.]")),
+          orElse: () => "",
+        );
+
+    final String pageNumber = word
+        .split("..")
+        .lastWhere(
+          (e) => e.contains(RegExp("[0-9.]")),
+          orElse: () => "",
+        )
+        .replaceAll('.', '');
+
+    final startIndex = word.indexOf(index);
+    final endIndex = word.indexOf(pageNumber);
+
+    final title = word
+        .substring(startIndex + index.length, endIndex)
+        .trim()
+        .replaceAll('.', '');
+
+    final top = Topic(
+      title: title,
+      index: index,
+      page: int.tryParse(pageNumber) ?? 0,
+    );
+
+    list.add(top);
+  }
+
+  return list;
+}
--- test/page_test.dart
+++ test/page_test.dart
@@ -1,83 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:stg/table.dart';
-
-class Chapter {
-  final String title;
-  final double index;
-  final int page;
-
-  Chapter({
-    required this.title,
-    required this.index,
-    required this.page,
-  });
-}
-
-class Topic {
-  final String title;
-  final double index;
-  final int? chapter;
-  final int page;
-
-  Topic({
-    required this.title,
-    required this.index,
-    this.chapter,
-    required this.page,
-  });
-
-  Map<String, dynamic> toJson() {
-    return {
-      "index": index,
-      "title": title,
-      "page": page,
-    };
-  }
-
-  @override
-  String toString() {
-    return toJson().toString();
-  }
-}
+import 'package:stg/handler.dart';
 
 void main() {
   test('should extract page titles,number correctly', () {
-    const str = tableOfContents;
+    final list = handlePages();
 
-    final list = str.split("\n");
-
-    for (var i in list) {
-      final word = i.trim();
-
-      final String index = word.split(" ").firstWhere(
-            (e) => e.contains(RegExp("[0-9.]")),
-            orElse: () => "",
-          );
-
-      if (word.isNotEmpty) {
-        print(index);
-
-        // final startIndex = word.indexOf(start);
-        // final firstDot = word.lastIndexOf(start);
-        // final lastDot = word.lastIndexOf('.') + 2;
-        // final end = word.substring(firstDot , lastDot);
-        //
-        // print('start: $start');
-        // print('dots: $end');
-        // final endIndex = word.indexOf(end, startIndex + start.length);
-        //
-        // final title =
-        //     word.substring(startIndex + start.length, endIndex).trim();
-        //
-        // final obj =
-        //     Topic(title: title, index: double.tryParse(start) ?? 0, page: 9);
-        //
-        // print(obj);
-
-        //break;
-      }
+    for (var element in list) {
+      print(element.toString());
     }
 
-    expect(str.length, str.length);
+    expect(list.length, list.length);
   });
 }
